5beeebeac057808ff65b0ad85a0187ed4fab4bb9
sheydaashoorian <sheydaashoorian@gmail.com>
2026-02-21 19:15:40 +0330

    edite task.service

---
+++ task-management-frontend/src/services/task.service.ts
@@ -0,0 +1,19 @@
+import api from '@/lib/api';
+import { ITask, CreateTaskDto } from '@/types/task';
+
+export const TaskService = {
+
+    getAllTasks: async (): Promise<ITask[]> => {
+    const { data } = await api.get<ITask[]>('/tasks');
+    return data;
+  },
+
+  createTask: async (taskData: CreateTaskDto): Promise<ITask> => {
+    const { data } = await api.post<ITask>('/tasks', taskData);
+    return data;
+  },
+
+  deleteTask: async (id: string): Promise<void> => {
+    await api.delete(`/tasks/${id}`);
+  }
+};
